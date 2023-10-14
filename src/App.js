615e8d59707ba7feb4f02c2ae1a027a1e7c0b84a
Johabela <johabbela@gmail.com>
2023-10-14 11:48:22 +1100

    ..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 // eslint-disable-next-line react-hooks/exhaustive-deps
 import './App.css';
-// eslint-disable-next-line react-hooks/exhaustive-deps
+
 function App() {
   return (
     <div className="App">
-
 
     </div>
   );
